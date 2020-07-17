625fc202d4726dea19d1871037fb0e392ff07724
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-07-17 16:47:01 -0300

    create widget animation for home screen top

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:app_animations/screens/home/widget/stagger_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 1;
+    
+    return StaggerAnimation(
+      controller: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
